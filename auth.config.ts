2bf6fc661fb455603a73b8a87478720f5b51d975
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-12-08 00:09:06 +0500

    auth config fixed

--- auth.config.ts
+++ auth.config.ts
@@ -1,35 +1,32 @@
 import type { NextAuthConfig } from "next-auth"
-import CredentialsProvider from 'next-auth/providers/credentials'
-import { prisma } from "./lib/prisma"
-import bcrypt from 'bcryptjs'
 
 export default {
   providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: {},
-        password: {},
-      },
-      async authorize(credentials) {
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email as any
-          }
-        });
+  //   CredentialsProvider({
+  //     name: 'credentials',
+  //     credentials: {
+  //       email: {},
+  //       password: {},
+  //     },
+  //     async authorize(credentials) {
+  //       const user = await prisma.user.findUnique({
+  //         where: {
+  //           email: credentials.email as any
+  //         }
+  //       });
 
-        if (!user) {
-          throw new Error("User Not Found!")
-        }
+  //       if (!user) {
+  //         throw new Error("User Not Found!")
+  //       }
 
-        const isPasswordMatched = bcrypt.compare(credentials.password as any, user.password);
+  //       const isPasswordMatched = bcrypt.compare(credentials.password as any, user.password);
 
-        if (!isPasswordMatched) {
-          throw new Error("Password Not Matched!");
-        }
+  //       if (!isPasswordMatched) {
+  //         throw new Error("Password Not Matched!");
+  //       }
 
-        return user;
-      },
-    })
+  //       return user;
+  //     },
+  //   })
   ],
 } satisfies NextAuthConfig
