abac32211461179abc625bc190d467818e05d63e
Noman Haider <152289775+nomanhaider99@users.noreply.github.com>
2024-12-23 17:13:51 +0500

    image update

--- components/ui/ProfileTrigger.tsx
+++ components/ui/ProfileTrigger.tsx
@@ -6,18 +6,23 @@ import { currentUser } from '@/app/actions/currentUser'
 
 const ProfileTrigger = async () => {
   const user = await currentUser();
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <div className='w-full rounded-full px-2 cursor-pointer flex items-center gap-2 border-[0.5px] border-zinc-500'>
-        <div><FaBars size={20} color='#000' /></div>
-        <div>
-            <Image
-                alt=''
-                src={user?.image || Avatar}
-                width={40}
-                height={40}
-                className=''
-             />
-        </div>
+      <div><FaBars size={20} color='#000' /></div>
+      <div>
+        <Image
+          alt='User Avatar'
+          src={user.image || Avatar}
+          width={40}
+          height={40}
+          className='rounded-full'
+        />
+      </div>
     </div>
   )
 }
